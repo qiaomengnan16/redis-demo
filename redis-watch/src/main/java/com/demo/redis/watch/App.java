301ee7483c0499bb26b21c9bb1f2827ad9469887
qiaomengnan16 <qiaomengnan2020@outlook.com>
2021-12-22 17:02:05 +0800

    redis watch

--- redis-watch/src/main/java/com/demo/redis/watch/App.java
+++ redis-watch/src/main/java/com/demo/redis/watch/App.java
@@ -7,30 +7,39 @@ import redis.clients.jedis.Transaction;
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
+        // key
         String key = "aData";
         Jedis jedis = JedisBuilder.buildJedis();
+        // 默认设置为0
         jedis.set(key, "0");
         CountDownLatch count = new CountDownLatch(10);
+        // 开10个线程对key做++操作，最终值应该是10
         for (int i = 0; i < 10; i++) {
             new Thread(() -> {
                 Jedis jedisA = JedisBuilder.buildJedis();
                 while (true) {
+                    // watch
                     jedisA.watch(key);
+                    // 获取并自增
                     int value = Integer.parseInt(jedisA.get(key));
                     value++;
+                    // 开启事务并传值
                     Transaction tx = jedisA.multi();
                     tx.set(key, value + "");
+                    // 成功退出循环
                     if (tx.exec() != null) {
                         System.out.println(Thread.currentThread().getName() + " 操作成功");
                         count.countDown();
                         break;
                     } else {
+                        // 不成功继续重试
                         System.out.println(Thread.currentThread().getName() + " 发生数据冲突，更新失败，重试");
                     }
                 }
             }).start();
         }
         count.await();
+        // 输出值
         System.out.println(jedis.get(key));
     }
 
